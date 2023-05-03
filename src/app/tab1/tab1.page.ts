1aeb8569b82e75ada1fe20f261f3d94781e64cc9
João Ferreira <joaoferreirape@gmail.com>
2023-05-02 22:33:18 -0300

    Aula 2024-05-02

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -4,26 +4,31 @@ import { ViacepService } from '../services/viacep.service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  inputCep: string = ''; // Campo de busca
+  textareaCep: string = ''; // Valor do textarea
+  dataKeys: any;
+  cep: string = ''; // Campo de saída do CEP
+  logradouro: string = ''; // Campor de saída do Logradouro
 
-  inputCep: string = '';
-  textareaCep: string = '';
-
-  constructor(private viacepService: ViacepService) { }
+  constructor(private viacepService: ViacepService) {}
 
   ngOnInit() {
-    this.viacepService.getRemoteData('01001000').subscribe(data => {
+    this.viacepService.getRemoteData('01001000').subscribe((data) => {
       console.log(data);
-    })
+    });
   }
 
   consultarCep() {
-    this.viacepService.getRemoteData(this.inputCep).subscribe(data => {
+    this.viacepService.getRemoteData(this.inputCep).subscribe((data) => {
       this.textareaCep = JSON.stringify(data);
+      this.cep = JSON.parse(JSON.stringify(data)).cep;
+      this.logradouro = JSON.parse(JSON.stringify(data)).logradouro;
       console.log(data);
-    })
+      this.dataKeys = Object.keys(data);
+      console.log(this.dataKeys);
+    });
   }
-
 }
